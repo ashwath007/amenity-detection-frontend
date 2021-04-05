541f2b964c2d05b07e88270afc1ae0e1186eb2cc
vicky <srivicky2000@gmail.com>
2021-04-05 13:37:41 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
+import React,{useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
-
+import FormData from 'form-data'
+import axios from 'axios'
 function App() {
+
+
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "",
+    stock: "",
+    photo: "",
+    categories: [],
+    category: "",
+    loading: false,
+    error: "",
+    createdProduct: "",
+    getaRedirect: false,
+    formData: new FormData()
+  });
+
+  const {
+    name,
+    description,
+    price,
+    stock,
+    categories,
+    category,
+    loading,
+    error,
+    createdProduct,
+    getaRedirect,
+    formData
+  } = values;
+
+
+  const handleChange = name => event => {
+    const value = name === "photo" ? event.target.files[0] : event.target.value;
+    formData.set(name, value);
+    setValues({ ...values, [name]: value });
+  };
+
+
+
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    return fetch(`/api`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+      body: formData
+    })
+      .then(response => {
+        return response.json();
+      })
+      .catch(err => console.log(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <form>
+      <span>Post photo</span>
+      <div className="form-group">
+        <label className="btn btn-block btn-success">
+          <input
+            onChange={handleChange("photo")}
+            type="file"
+            name="photo"
+            accept="image"
+            placeholder="choose a file"
+          />
+        </label>
+      </div>
+      <button
+        type="submit"
+        onClick={onFormSubmit}
+        className="btn btn-outline-success mb-3"
+      >
+        Create Product
+      </button>
+      </form>
+      </div>
     </div>
   );
 }
